e1e18fb60c29bc237a63fde20f7bcdddf04c7fea
Benny Ling <sln98106@gmail.com>
2017-08-27 09:15:22 +0800

    fix require path

--- includes/google_drive/decrypt.php
+++ includes/google_drive/decrypt.php
@@ -1,5 +1,5 @@
 <?php
-require_once("pass.php");
+require_once(dirname(__FILE__)."/pass.php");
 
 $gd_decrypt = openssl_decrypt(file_get_contents(dirname(__FILE__)."/client_secret.json.aes"), "AES-256-CBC", $gd_pass, 0, substr(hash("sha512", $gd_pass), 0, 16));
 $gd_client = json_decode($gd_decrypt, true)["installed"];
